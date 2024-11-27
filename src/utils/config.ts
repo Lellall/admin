c3f2431e7d9e0833350147fc20037bad8dfc7885
Mujtaba <mujtaba.adamu@flexisaf.com>
2024-11-28 00:00:28 +0100

    point back to dev

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    BACKEND_URL: "https://api.lellall.com",
+    BACKEND_URL: "https://api.dev.lellall.com/backend-core/",
   },
   production: {
     BACKEND_URL: "https://api.lellall.com",
