23125016796b8df429e2192d526bf4a551cb8a31
Mujtaba <mujtaba.adamu@flexisaf.com>
2024-11-29 23:00:46 +0100

    hot fix dev to prod url

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    BACKEND_URL: "https://api.dev.lellall.com",
+    BACKEND_URL: "https://api.lellall.com",
   },
   production: {
     BACKEND_URL: "https://api.lellall.com",
@@ -11,5 +11,4 @@ const currentEnv = process.env.NODE_ENV || "development"
 
 export const configUrl = config[currentEnv as "development" | "production"]
 
-// export const { BACKEND_URL } = config[currentEnv]
-// export const BACKEND_URL = (config as { [key: string]: { BACKEND_URL: string } })[currentEnv].BACKEND_URL;
+// https://api.dev.lellall.com/backend-core/
